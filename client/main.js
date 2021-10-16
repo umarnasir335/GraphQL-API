488ab6c3d0615b4eb931f4025a69a72a75dcbe7e
Umar Nasir <umarnasir335@gmail.com>
2021-10-15 23:49:07 -0700

    added main file for client

---
+++ client/main.js
@@ -0,0 +1,55 @@
+const GRAPHQL_API = 'http://localhost:4000/';
+const todosElement = document.querySelector('#todos');
+const form = document.querySelector('form');
+const titleInput = document.querySelector('#title');
+
+const query = `{
+  allTodos {
+    id
+    title
+    done
+  }
+}`;
+
+fetch(GRAPHQL_API, {
+  method: 'POST',
+  body: JSON.stringify({
+    query
+  }),
+  headers: {
+    'content-type': 'application/json'
+  }
+}).then(response => response.json())
+  .then(result => {
+    const todos = result.data.allTodos;
+
+    todosElement.innerHTML = todos.reduce((html, todo) => {
+      return html + `<li>${todo.title}</li>`;
+    }, '');
+  });
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const title = titleInput.value;
+  const query = `
+  mutation {
+    createTodo(title: "${title}", done: false) {
+      id
+      title
+      done
+    }
+  }`;
+
+  fetch(GRAPHQL_API, {
+    method: 'POST',
+    body: JSON.stringify({
+      query
+    }),
+    headers: {
+      'content-type': 'application/json'
+    }
+  }).then(response => response.json())
+    .then(result => {
+      todosElement.innerHTML += `<li>${title}</li>`;
+    });
+});
