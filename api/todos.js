fdf4e030407ec5a7eef00e09a30de2a469004931
Umar Nasir <umarnasir335@gmail.com>
2021-10-21 21:50:45 -0700

    added api export module

---
+++ api/todos.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    id: 1,
+    title: 'Learn GraphQL',
+    done: false
+  }, {
+    id: 2,
+    title: 'Learn nanoql',
+    done: false
+  }, {
+    id: 3,
+    title: 'Learn Schemas',
+    done: false
+  }];
