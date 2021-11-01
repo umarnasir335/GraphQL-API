1168e36b875d1e179ff9dad824ad651199bdc08c
Umar Nasir <umarnasir335@gmail.com>
2021-10-31 23:03:50 -0700

    added home page to client

---
+++ client/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class home extends Component {
+    render() {
+        return (
+            <div>
+                
+                const homePage = { (hPage) => {
+                        
+                        }
+                    }
+
+            </div>
+        )
+    }
+}
