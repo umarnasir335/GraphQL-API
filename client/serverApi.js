7fb9b4cdad5d27fee39e5f6e96e11dc231d3a432
Umar Nasir <umarnasir335@gmail.com>
2021-11-15 22:03:42 -0800

    added intermediate app for post req test

---
+++ client/serverApi.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class serverApi extends Component {
+    render() {
+        return (
+            <div>
+                var app = require('express')();
+                    var bodyParser = require('body-parser');
+
+                    app.use(bodyParser.json()); // for parsing application/json
+                    app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+                    app.post('/data', function (req, res) {
+                    console.log(req.body);
+                    res.end();
+                    });
+
+app.listen(3000);
+            </div>
+        )
+    }
+}
