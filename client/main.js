15a4aed90f9f33b60c790a5d934347d7f8267191
Umar Nasir <umarnasir335@gmail.com>
2021-11-21 21:56:24 -0800

    fetch req GraphQL

--- client/main.js
+++ client/main.js
@@ -52,4 +52,25 @@ form.addEventListener('submit', (event) => {
     .then(result => {
       todosElement.innerHTML += `<li>${title}</li>`;
     });
+
+    
+fetch(GRAPHQL_API, {
+  method: 'POST',
+  body: JSON.stringify({
+    query
+  }),
+  headers: {
+    'content-type': 'application/json'
+  }
+}).then(response => response.json())
+  .then(result => {
+    const todos = result.data.allTodos;
+
+    todosElement.innerHTML = todos.reduce((html, todo) => {
+      return html + `<li>${todo.title}</li>`;
+    }, '');
+  });
+
+
+  
 });
