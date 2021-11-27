4d829fbd4e061b3229def0c8928a42ce302cf088
Umar Nasir <umarnasir335@gmail.com>
2021-11-26 23:09:58 -0800

    added GraphQL req

--- client/main.js
+++ client/main.js
@@ -69,6 +69,20 @@ fetch(GRAPHQL_API, {
     todosElement.innerHTML = todos.reduce((html, todo) => {
       return html + `<li>${todo.title}</li>`;
     }, '');
+
+    fetch(GRAPHQL_API, {
+      method: 'POST',
+      body: JSON.stringify({
+        query
+      }),
+      headers: {
+        'content-type': 'application/json'
+      }
+    }).then(response => response.json())
+      .then(result => {
+        todosElement.innerHTML += `<li>${title}</li>`;
+      });
+  });
   });
 
 
