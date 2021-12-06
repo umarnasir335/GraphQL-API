1812c15051ab98a82e00d57a5e44478880eaa74d
Umar Nasir <umarnasir335@gmail.com>
2021-12-05 17:30:59 -0800

    updated tests

--- client/app.test.js
+++ client/app.test.js
@@ -3,6 +3,13 @@ const supertest = require('supertest');
 const app = require('./app');
 const project = require('./constants/project');
 
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/GRAPHQL/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
 describe('GET /', () => {
   it('should respond with a message', async () => {
     const response = await supertest(app)
