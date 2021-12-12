9dd2520bae1c73f12815d4f5bd13b63382799c4d
Umar Nasir <umarnasir335@gmail.com>
2021-12-11 23:12:17 -0800

    updated tests

--- client/test.js
+++ client/test.js
@@ -19,6 +19,17 @@ export default class test extends Component {
                         expect(response.body.message).toEqual(project.message);
                     });
             });
+            
+                        describe('GET /api/v1/companies', () => {
+                        it('should respond with an array of companies', async () => {
+                            const response = await supertest(app)
+                            .get('/api/v1/companies')
+                            .expect('Content-Type', /json/)
+                            .expect(200);
+
+                            expect(response.body).toBeInstanceOf(Array);
+                        });
+});
             </div>
         )
     }
