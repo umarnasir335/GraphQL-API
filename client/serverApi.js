cd4574407095d18b80ddb4c21310a9ca65bb7b4f
Umar Nasir <umarnasir335@gmail.com>
2021-12-13 21:29:13 -0800

    changed  api funct

--- client/serverApi.js
+++ client/serverApi.js
@@ -14,6 +14,17 @@ export default class serverApi extends Component {
                     console.log(req.body);
                     res.end();
                     });
+                    describe('GET /api/v1/states', () => {
+                        
+                it('should respond with an array of states', async () => {
+                    const response = await supertest(app)
+                    .get('/api/v1/states')
+                    .expect('Content-Type', /json/)
+                    .expect(200);
+
+                    expect(response.body.length).toBeGreaterThan(0);
+                });
+                                    
 
 app.listen(3000);
             </div>
