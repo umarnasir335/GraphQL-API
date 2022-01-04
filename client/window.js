94d0f5042a6d987be1d4c064351768aa8ab5d791
Umar Nasir <umarnasir335@gmail.com>
2022-01-03 21:02:45 -0800

    added window functs

--- client/window.js
+++ client/window.js
@@ -61,4 +61,27 @@ function createWindow () {
         preload: path.join(__dirname, 'preload.js')
       }
     });
+    app.on('window-all-closed', function () {
+        // On macOS it is common for applications and their menu bar
+        // to stay active until the user quits explicitly with Cmd + Q
+        if (process.platform !== 'darwin') app.quit();
+      });
+      
+      app.on('web-contents-created', (e, contents) => {
+        contents.on('will-navigate', (event, url) => {
+          event.preventDefault();
+          console.log('blocked navigate:', url);
+        });
+        contents.on('new-window', async (event, url) => {
+          event.preventDefault();
+          console.log('blocked window:', url);
+        });
+      });
+      
+      app.on('activate', function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (mainWindow === null) createWindow();
+      });
+      
 }
