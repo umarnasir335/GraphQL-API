9a393a7667532724b326377ab509e606df7960b3
Umar Nasir <umarnasir335@gmail.com>
2022-01-08 22:58:47 -0800

    window states

--- client/window.js
+++ client/window.js
@@ -37,6 +37,25 @@ function createWindow () {
     
   }
 
+  describe('GET /api/v1/states', () => {
+    it('should respond with an array of states', async () => {
+      const response = await supertest(app)
+        .get('/api/v1/states')
+        .expect('Content-Type', /json/)
+        .expect(200);
+  
+      expect(response.body.length).toBeGreaterThan(0);
+    });
+  
+    it('should respond with an individual state', async () => {
+      const response = await supertest(app)
+        .get('/api/v1/states/1')
+        .expect('Content-Type', /json/)
+        .expect(200);
+  
+      expect(response.body.id).toBe(1);
+    });
+  
   function createHighlightWindow (message) {
     if (highlightWindow) {
       return;
