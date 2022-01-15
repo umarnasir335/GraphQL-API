f7b2bdea26790e978287682f2e905053712aeaab
Umar Nasir <umarnasir335@gmail.com>
2022-01-14 21:26:41 -0800

    server init

--- client/logger.js
+++ client/logger.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react'
 
 export default class logger extends Component {
     render() {
+
+        function init(server) {
+            const teamTimeoutMS = 30000;
+            const io = socketIO(server);
+          
+            const serverState = {
+              currentWord: getRandomWord(),
+            };
+          
         it('should respond with a 404 for a not found state', async () => {
             await supertest(app)
               .get('/api/v1/states/4200')
@@ -11,12 +20,12 @@ export default class logger extends Component {
         return (
             <div>
                      console[type](
-    ...args.map((item) => {
+                         ...args.map((item) => {
                 if (typeof item === 'object') {
-             return util.inspect(item, { depth: 5, colors: true });
+                    return util.inspect(item, { depth: 5, colors: true });
                 }
-      return item;
-    })
+                return item;
+                })
   );    
             </div>
         )
